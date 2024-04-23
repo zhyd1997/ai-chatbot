8d05bad75203cf0b04c647290e9669cce643cc22
Yadong Zhang <zhyd007@gmail.com>
2024-04-23 15:02:27 +0800

    Update page.tsx

--- app/(chat)/chat/[id]/page.tsx
+++ app/(chat)/chat/[id]/page.tsx
@@ -7,6 +7,8 @@ import { Chat } from '@/components/chat'
 import { AI } from '@/lib/chat/actions'
 import { Session } from '@/lib/types'
 
+export const maxDuration = 120
+
 export interface ChatPageProps {
   params: {
     id: string
