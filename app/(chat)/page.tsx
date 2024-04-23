134d0775ff3d807160b7e698a75963c8a3d68640
Yadong Zhang <zhyd007@gmail.com>
2024-04-23 15:08:15 +0800

    Update page.tsx

--- app/(chat)/page.tsx
+++ app/(chat)/page.tsx
@@ -5,7 +5,7 @@ import { auth } from '@/auth'
 import { Session } from '@/lib/types'
 import { getMissingKeys } from '../actions'
 
-export const maxDuration = 120
+// export const maxDuration = 120
 
 export const metadata = {
   title: 'Next.js AI Chatbot'
